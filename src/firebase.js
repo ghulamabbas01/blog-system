717c36b10199ea6cc86413ee7d4e46f718d849eb
Abbas Khan <ak6119231@gmail.com>
2023-09-12 13:54:56 +0500

    sd

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC-rNqWhvCIIxZkbop7ya5UOpljgKGRrUY",
+  authDomain: "test-firebase-7f37e.firebaseapp.com",
+  projectId: "test-firebase-7f37e",
+  storageBucket: "test-firebase-7f37e.appspot.com",
+  messagingSenderId: "442520417734",
+  appId: "1:442520417734:web:af332724f4fa7183b9aec7",
+  databaseUrl: "https://test-firebase-7f37e-default-rtdb.firebaseio.com",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
